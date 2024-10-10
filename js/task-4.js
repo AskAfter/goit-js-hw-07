8806bc92f34e49a438a0f9a774fa9bcf65e4b7c3
AskAfter <askafter904@gmail.com>
2024-10-10 03:25:32 +0200

    task-4 it was harder than steel

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,30 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', funcSubmit);
+
+const textInput = document.querySelectorAll('.login-form input');
+
+const userData = {};
+
+textInput.forEach(input => {
+  input.addEventListener('focus', () => {
+    input.placeholder = 'Type area';
+  });
+  input.addEventListener('blur', () => {
+    input.placeholder = '';
+  });
+});
+
+function funcSubmit(event) {
+  event.preventDefault();
+  const submitForm = event.target;
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+  if (email === '' || password === '') {
+    return console.log('All form fields must be filled in');
+  }
+  userData.email = email.trim();
+  userData.password = password.trim();
+  console.log(userData);
+  form.reset();
+}
