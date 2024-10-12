b2c310d32052dbeab140b8ff238865fe712f015c
AskAfter <askafter904@gmail.com>
2024-10-12 23:45:08 +0200

    task-6 done

--- js/task-6.js
+++ js/task-6.js
@@ -18,26 +18,35 @@ destroy.addEventListener('mouseout', () => {
   destroy.style.backgroundColor = '#FF4E4E';
 });
 
-const btnCreate = create.addEventListener('click', () => {
+create.addEventListener('click', () => {
   const inputValue = input.value;
+
+  if (isNaN(inputValue) || inputValue < 1 || inputValue > 100) {
+    alert('Please enter a number between 1 and 100');
+    return;
+  }
+
   createBoxes(inputValue);
 });
 
 function createBoxes(amount) {
+  boxes.innerHTML = '';
+
   let heightWidth = 30;
-  if (amount >= 1 && amount <= 100) {
-    boxes.innerHTML = '';
-    for (let i = 1; i <= amount; i++) {
-      const box = document.createElement('div');
-      box.style.width = `${heightWidth}px`;
-      box.style.height = `${heightWidth}px`;
-      box.style.backgroundColor = getRandomHexColor();
-      box.classList.add('boxes');
-      heightWidth += 10;
-      boxes.appendChild(box);
-    }
+  let boxesHTML = '';
+
+  for (let i = 1; i <= amount; i++) {
+    const color = getRandomHexColor();
+    boxesHTML += `<div style="width: ${heightWidth}px; height: ${heightWidth}px; background-color: ${color}; margin: 5px;"></div>`;
+    heightWidth += 10;
   }
+
+  boxes.insertAdjacentHTML('beforeend', boxesHTML);
 }
+
+destroy.addEventListener('click', () => {
+  boxes.innerHTML = '';
+});
 
 const btnDestroy = destroy.addEventListener('click', () => {
   boxes.innerHTML = '';
